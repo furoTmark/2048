390ab03fcbaa2f873ed5b45ddcced3e74f343d93
Chris Love <clove@extremewebworks.com>
2017-08-28 13:55:19 -0400

    update the manifest and logo files.

--- sw.js
+++ sw.js
@@ -33,4 +33,16 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("fetch", function (event) {
 
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request);
+        })
+    );
+
 });
