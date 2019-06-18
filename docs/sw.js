292b9bf51fe52c1df431e52cb02919e0c9dca78a
Tamas Furo <Tamas.Furo@msg.group>
2019-06-18 18:33:10 +0300

    updated paths

--- docs/sw.js
+++ docs/sw.js
@@ -1,15 +1,15 @@
 const version = "1.03",
 preCache = "PRECACHE-" + version,
     cacheList = [
-    "/",
-    "style/main.css",
-    "js/keyboard_input_manager.js",
-    "js/html_actuator.js",
-    "js/grid.js",
-    "js/tile.js",
-    "js/local_storage_manager.js",
-    "js/game_manager.js",
-    "js/application.js"
+    "2048/",
+    "2048/style/main.css",
+    "2048/js/keyboard_input_manager.js",
+    "2048/js/html_actuator.js",
+    "2048/js/grid.js",
+    "2048/js/tile.js",
+    "2048/js/local_storage_manager.js",
+    "2048/js/game_manager.js",
+    "2048/js/application.js"
 ];
 
 /*  Service Worker Event Handlers */
