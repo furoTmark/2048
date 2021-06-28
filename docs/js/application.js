5334d545768247d4c7a8091bc20e97890551f798
Furo Tamas Mark <furo.tamas.mark@gmail.com>
2021-06-28 17:12:27 +0300

    fixes

--- docs/js/application.js
+++ docs/js/application.js
@@ -1,37 +1,9 @@
-"use strict"; //https://love2dev.com/blog/javascript-strict-mode/
+"use strict"; 
 ( function () {
 
   // Wait till the browser is ready to render the game (avoids glitches)
   window.requestAnimationFrame( function () {
     new GameManager( 4, KeyboardInputManager, HTMLActuator, LocalStorageManager );
-  } );
-
-  addToHomescreen( {
-    autostart: true,
-    autoHide: 0,
-    logging: true,
-    minSessions: 2,
-    onShow: function () {
-      console.log( "showing" );
-    },
-    onInit: function () {
-      console.log( "initializing" );
-    },
-    onAdd: function () {
-      console.log( "adding" );
-    },
-    onInstall: function () {
-      console.log( "Installing" );
-    },
-    onCancel: function () {
-      console.log( "Cancelling" );
-    },
-    displayNextPrime: true,
-    customCriteria: true,
-    displayPace: 0,
-    customPrompt: {
-      src: "2048/meta/2048-logo-48x48.png"
-    }
   } );
 
 } )();
